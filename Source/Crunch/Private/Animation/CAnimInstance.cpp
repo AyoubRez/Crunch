7653f00a466ed73432dd7b8fef46040e29cefd39
AyoubREZZOUK <rezzouk.ayouble@gmail.com>
2026-02-15 15:36:28 +0100

    feat: Setup Movement Animation with leaning

---
+++ Source/Crunch/Private/Animation/CAnimInstance.cpp
@@ -0,0 +1,45 @@
+// // Copyright (c) 2025 YokaiProject inc.
+
+
+#include "Animation/CAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UCAnimInstance::NativeInitializeAnimation()
+{
+	UE_LOG(LogTemp, Log, TEXT("CAnimInstance::NativeInitializeAnimation"));
+	OwnerCharacter = Cast<ACharacter>(GetOwningActor());
+	if (OwnerCharacter)
+	{
+		UE_LOG(LogTemp, Log, TEXT("CAnimInstance::NativeInitializeAnimation: OwnerCharacter found"));
+		OwnerCharacterMovementComp = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UCAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	UE_LOG(LogTemp, Log, TEXT("CAnimInstance::NativeUpdateAnimation"));
+	if (OwnerCharacter)
+	{
+		MovementSpeed = OwnerCharacter->GetVelocity().Length();
+		FRotator Rotation = OwnerCharacter->GetActorRotation();
+		FRotator DeltaRotation = UKismetMathLibrary::NormalizedDeltaRotator(
+			Rotation, LastRotation
+		);
+		LastRotation = Rotation;
+
+		YawSpeed = DeltaRotation.Yaw / DeltaSeconds;
+		SmoothedYawSpeed =
+			UKismetMathLibrary::FInterpTo(
+				SmoothedYawSpeed,
+				YawSpeed,
+				DeltaSeconds,
+				YawSpeedSmoothLerpSpeed);
+	}
+}
+
+void UCAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	UE_LOG(LogTemp, Log, TEXT("CAnimInstance::NativeThreadSafeUpdateAnimation"));
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+}
---
+++ Source/Crunch/Private/Animation/CAnimInstance.h
@@ -0,0 +1,55 @@
+// // Copyright (c) 2025 YokaiProject inc.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UCAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	/** Start UAnimInstance **/
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+	/** End UAnimInstance **/
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetMovementSpeed() const { return MovementSpeed; }
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool IsMoving() const { return MovementSpeed != 0.f; }
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool IsNotMoving() const { return MovementSpeed == 0.f; }
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetYawSpeed() const { return YawSpeed; }
+	
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetSmoothedYawSpeed() const { return SmoothedYawSpeed; }
+
+	
+private:
+	UPROPERTY()
+	ACharacter* OwnerCharacter;
+
+	UPROPERTY()
+	class UCharacterMovementComponent* OwnerCharacterMovementComp;
+
+	float MovementSpeed;
+	float YawSpeed;
+	float SmoothedYawSpeed;
+
+	UPROPERTY(EditAnywhere, Category = "Animation")
+	float YawSpeedSmoothLerpSpeed = 1.f;
+	
+	FRotator LastRotation;
+};
